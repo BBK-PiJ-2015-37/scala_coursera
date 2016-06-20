7feb406c936eb1656812dcea8e6e31bbefad9726
janss001 <janss001@gmail.com>
2016-06-20 17:31:06 +0100

    Implemented and tested forall()

--- funsets/funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -78,7 +78,10 @@ class FunSetSuite extends FunSuite {
     val s2 = singletonSet(2)
     val s3 = singletonSet(3)
     val s4 = singletonSet(2)
-    val s5 = singletonSet(4)
+    val s5 = singletonSet(-4)
+    val s6 = singletonSet(999)
+    val s7 = singletonSet(421)
+    val s8 = singletonSet(-876)
   }
 
   /**
@@ -146,10 +149,24 @@ class FunSetSuite extends FunSuite {
       val filter1 = filter(f3, x => x % 2 == 0)
       val filter2 = filter(f3, x => x < 4)
       assert(contains(filter1, 2), "Set of even numbers does not contain 2")
-      assert(contains(filter1, 4), "Set of even numbers does not contain 4")
+      assert(contains(filter1, -4), "Set of even numbers does not contain 4")
       assert(!contains(filter1, 1), "Set of even numbers unexpectedly contains 1")
       assert(!contains(filter2, 4), "Set of numbers less than 4 unexpectedly contains 4")
       assert(contains(filter2, 1), "Set of numbers less than 4 does not contain 1")
+    }
+  }
+
+  test("forall functions correctly checks all values from -1000 to 1000 against a predicate") {
+    new TestSets {
+      val f1 = union(s1, s3) // 1,3
+      val f2 = union(s2, s4) // 2,2
+      val f3 = union(s5, s8) // -4, -876
+      val f4 = union(s6, s7) // 999, 421
+      val f5 = union(f1, f4) // all odd
+      val f6 = union(f2, f3) // all even
+      assert(forall(f6, x => x % 2 == 0), "Not all elements in f6 are even")
+      assert(!forall(f5, x => x % 2 == 0), "Not all elements in f5 are odd")
+      assert(forall(f3, x => x < 0), "Not all elements in f3 are less than zero")
     }
   }
 
